ad59e7436f6f6013888f423ea0351502273569b2
Phumudzo98 <phumu98@gmail.com>
2025-09-01 21:51:41 +0200

    images

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SelectAFarmMainComponent } from './pages/select-a-farm-main/select-a-farm-main.component';
+import { RegisterComponent } from './pages/register/register.component';
+import path from 'path';
+import { LoginComponent } from './pages/login/login.component';
+import { AddFarmComponent } from './pages/add-farm/add-farm.component';
 
 const routes: Routes = [
 
   {path: 'dashboard', component: DashboardComponent},
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    {path:'select-a-farm', component: SelectAFarmMainComponent},
+    {path:'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'add-farm', component: AddFarmComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SelectAFarmMainComponent } from './pages/select-a-farm-main/select-a-farm-main.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AddFarmComponent } from './pages/add-farm/add-farm.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    SelectAFarmMainComponent,
+    LoginComponent,
+    RegisterComponent,
+    AddFarmComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
